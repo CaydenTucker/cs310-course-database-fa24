3f1004c64672a919c2f482eeb4b394ceb4f9767f
CaydenT <ctucker18@stu.jsu.edu>
2024-09-29 08:14:09 -0500

    Added code to DAOUtility

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import com.github.cliftonlabs.json_simple.*;
 import java.util.ArrayList;
 
+
 public class DAOUtility {
     
     public static final int TERMID_FA24 = 1;
@@ -16,8 +17,39 @@ public class DAOUtility {
         
             if (rs != null) {
 
-                // INSERT YOUR CODE HERE
-
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+                
+                ArrayList<ArrayList<Object>> allRows = new ArrayList();
+                
+                while (rs.next()){
+                    ArrayList<Object> row = new ArrayList<>();
+                    
+                    for (int i = 1; i <= columnCount; i++){
+                        Object columnValue  = rs.getObject(i);
+                        
+                        row.add(columnValue);
+                    }
+                    
+                    allRows.add(row);
+                }
+                
+                for (ArrayList<Object> row : allRows){
+                    JsonObject record = new JsonObject();
+                    
+                    for (int i = 0; i < columnCount; i++){
+                        String columnName = rsmd.getColumnName(i + 1);
+                        Object columnValue = row.get(i);
+                        
+                        if (columnValue == null){
+                            record.put(columnName, null);
+                        } else {
+                            record.put(columnName, columnValue.toString());
+                        }
+                    }
+                    
+                    records.add(record);
+                }
             }
             
         }
